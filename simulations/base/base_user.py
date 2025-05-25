6ce439c8e57238e464772b02a09beb5e3c16a4f6
Ahmed Madyan <ahmed.madyan01@gmail.com>
2025-05-26 01:30:40 +0300

    Update base_user.py

--- simulations/base/base_user.py
+++ simulations/base/base_user.py
@@ -1,18 +1,19 @@
 from locust import SequentialTaskSet
 from request_builder.RequestBuilder import RequestBuilder
 from reponse_validator.ResponseValidator import ResponseValidator
+from config import SERVICE_BASE_URI
 
 
 class BaseTaskSet(SequentialTaskSet):
     """Base class for all TaskSet implementations that provides common initialization logic."""
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bearer_token = None
 
-    def on_start(self, host=None):
+    def on_start(self):
         """Initialize any user-specific data when the user starts."""
         self.request_builder = RequestBuilder(self)
+        self.request_builder.with_host(SERVICE_BASE_URI["DUMMY_JSON_BASE_URI"])
         # Set the host URL once during initialization
-        self.request_builder.with_host(self.host)
-        self.validator = ResponseValidator() 
+        self.validator = ResponseValidator()
